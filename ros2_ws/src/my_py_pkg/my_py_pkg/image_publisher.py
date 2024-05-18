2994408fead3b946aacec2c4315b4c890794f190
Yvan Pearson <ypearson@theaiinstitute.com>
2024-05-17 23:49:43 -0400

    name change

--- ros2_ws/src/my_py_pkg/my_py_pkg/image_publisher.py
+++ ros2_ws/src/my_py_pkg/my_py_pkg/image_publisher.py
@@ -1,6 +1,6 @@
 import rclpy
 from rclpy.node import Node
-from my_robot_interfaces.msg import CustomImage
+from my_robot_interfaces.msg import GraphNodeData
 from sensor_msgs.msg import Image
 from std_msgs.msg import Header
 from cv_bridge import CvBridge
@@ -9,7 +9,7 @@ import cv2
 class ImagePublisher(Node):
     def __init__(self):
         super().__init__('image_publisher')
-        self.publisher_ = self.create_publisher(CustomImage, 'image_topic', 10)
+        self.publisher_ = self.create_publisher(GraphNodeData, 'image_topic', 10)
         self.timer = self.create_timer(0.5, self.timer_callback)
         self.bridge = CvBridge()
         self.image_path = 'src/my_py_pkg/my_py_pkg/bike.jpg'
@@ -21,15 +21,15 @@ class ImagePublisher(Node):
         if img is not None:
             img_msg = self.bridge.cv2_to_imgmsg(img, encoding='bgr8')
 
-            custom_msg = CustomImage()
-            custom_msg.header = Header()
-            custom_msg.header.stamp = self.get_clock().now().to_msg()
-            custom_msg.image = img_msg
-            custom_msg.gnode_name = 'example_node_name'
-            custom_msg.gnode_edges = ['edge1', 'edge2', 'edge3']
+            graph_node_data_msg = GraphNodeData()
+            graph_node_data_msg.header = Header()
+            graph_node_data_msg.header.stamp = self.get_clock().now().to_msg()
+            graph_node_data_msg.image = img_msg
+            graph_node_data_msg.gnode_name = 'example_node_name'
+            graph_node_data_msg.gnode_edges = ['edge1', 'edge2', 'edge3']
 
-            self.publisher_.publish(custom_msg)
-            self.get_logger().info('Publishing custom image message.')
+            self.publisher_.publish(graph_node_data_msg)
+            self.get_logger().info('Publishing graph_node_data image message.')
         else:
             self.get_logger().error(f'Failed to read image from {self.image_path}')
 
